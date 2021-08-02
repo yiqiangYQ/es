6c5108a1a760f7837cb179f6bdd84be0d15cc2d2
yiqiang <645442713@qq.com>
2021-08-02 08:10:11 +0800

    first

---
+++ src/main/java/com/my/EsTest.java
@@ -0,0 +1,52 @@
+package com.my;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+
+import java.io.IOException;
+
+/**
+ * @Author: yiqiang
+ * @Date: 2021/7/25 21:15
+ * @Description:
+ */
+public class EsTest {
+    public static void main(String[] args) throws IOException {
+        //创建es客户端
+        RestHighLevelClient esclient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",9200,"http"))
+        );
+
+
+        //创建索引
+        /*CreateIndexRequest users = new CreateIndexRequest("users");
+        CreateIndexResponse response = esclient.indices().create(users, RequestOptions.DEFAULT);
+        boolean acknowledged = response.isAcknowledged();
+        System.out.println(acknowledged);*/
+
+        //查询索引
+        /*GetIndexRequest users1 = new GetIndexRequest("users");
+        GetIndexResponse getIndexResponse = esclient.indices().get(users1, RequestOptions.DEFAULT);
+        System.out.println(getIndexResponse.getAliases());
+        System.out.println(getIndexResponse.getMappings());
+        System.out.println(getIndexResponse.getSettings());*/
+
+        //删除索引
+        /*AcknowledgedResponse users = esclient.indices().delete(new DeleteIndexRequest("users"), RequestOptions.DEFAULT);
+        System.out.println(users.isAcknowledged());*/
+
+
+
+
+        //关闭es
+        esclient.close();
+    }
+}
